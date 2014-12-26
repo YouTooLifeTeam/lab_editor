aeb6519ee036c787bc9b225635536ea9f9028ffc
YouTooLife <youtoolifesite@ya.ru>
2014-12-26 23:55:41 +0700

    Create new Project

---
+++ desktop/src/com/youtoolife/lab_editor/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.youtoolife.lab_editor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.youtoolife.lab_editor.LabEditor;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LabEditor(), config);
+	}
+}
